e392b82fd1f0be9cccb419219454a79656d9314d
Jonathan Ato Markin <jona10711@gmail.com>
2025-05-02 23:01:11 +0200

    Initial setup

---
+++ src/main/java/com/markin/gitops_approval/GitopsApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.markin.gitops_approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitopsApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitopsApprovalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markin/gitops_approval/GitopsApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markin.gitops_approval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitopsApprovalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
